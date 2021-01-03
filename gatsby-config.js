667826ef100fcbe5a016c888c7f1058b8af11333
VITALIY1986 <staslego1986@gmail.com>
2021-01-03 20:00:00 +0200

    asd

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
   {
     resolve: `gatsby-plugin-react-helmet-canonical-urls`,
     options: {
-      siteUrl: `http://www.svadebnyye-platya.kiev.ua`,
+      siteUrl: `http://www.svadebnyye-platya.kiev.ua/`,
     },
   },
   
