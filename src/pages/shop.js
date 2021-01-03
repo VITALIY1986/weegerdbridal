d95aeffc349020a3b68da8eb7decf089638d3f16
VITALIY1986 <staslego1986@gmail.com>
2021-01-03 20:39:51 +0200

    asd

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -16,7 +16,7 @@ class BlogIndex extends React.Component {
 
   return (
     <Layout location={this.props.location} title={siteTitle}>
-      <SEO title="All posts" />
+      <SEO title="Коллекция свадебных платьев" />
 
       <Bo />
       <div class="container">
